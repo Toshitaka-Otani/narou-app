3062487ef5a65d5f38b665bb7b1db5ad56c8dda6
ToshitakaOtani <v.t.tanitani@gmail.com>
2021-03-30 01:39:16 +0900

    first commit

---
+++ pages/main.tsx
@@ -0,0 +1,34 @@
+import {
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+} from "@chakra-ui/react";
+import React from "react";
+
+export default function Main() {
+  return (
+    <>
+      <Tabs size="md" variant="enclosed">
+        <TabList>
+          <Tab>更新された作品</Tab>
+          <Tab>なろうランキング</Tab>
+          <Tab>作品検索</Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel>
+            <p>登録された作品の更新日をここにいれるよ</p>
+          </TabPanel>
+          <TabPanel>
+            <p>なろうのランキングをここにだして面白そうなものを読むよ</p>
+          </TabPanel>
+          <TabPanel>
+            <p>作品の検索をするよ</p>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+    </>
+  );
+}
+
