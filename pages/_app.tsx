7bf06f66f44caac6d8a56e60b107e266f54b4658
ToshitakaOtani <v.t.tanitani@gmail.com>
2022-06-26 01:53:05 +0900

    package version up && type fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import { ChakraProvider } from "@chakra-ui/react";
-import { AppProps } from "next/dist/next-server/lib/router/router";
+import { AppProps } from "next/app";
 import React from "react";
 import { Layout } from "../components/Layout";
 
