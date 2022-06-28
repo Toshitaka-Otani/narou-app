96575316a870da97ef59dc2488b9c5ff26bce3c7
ToshitakaOtani <v.t.tanitani@gmail.com>
2022-06-29 02:14:27 +0900

    fix

--- axios.ts
+++ axios.ts
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: process.env.NODE_ENV
-    ? `${process.env.NEXT_PUBLIC_API_BASE_URL}`
-    : "",
+  baseURL:
+    process.env.NODE_ENV == "development"
+      ? `${process.env.NEXT_PUBLIC_API_BASE_URL}`
+      : "",
   responseType: "json",
   headers: {
     "Content-Type": "application/json",
