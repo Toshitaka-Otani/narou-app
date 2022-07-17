880ca18bb211d7a0b4bd5f28ee2e57a29f86da10
ToshitakaOtani <v.t.tanitani@gmail.com>
2022-07-18 02:48:17 +0900

    エラー処理、エンターキーで検索結果表示

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,27 +1,52 @@
-import { Box, Input, Stack, Text, Button } from "@chakra-ui/react";
+import { Box, Input, Stack, Text, Button, useToast } from "@chakra-ui/react";
 import axios from "../axios";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { BookData } from "../components/BookData";
 
 export default function Search(): JSX.Element {
   const [searchText, setSearchText] = useState<string>("");
   const [books, setBooks] = useState<NarouBookData[]>([]);
+  const [errorToastMessage, setErrorToastMessage] = useState(undefined);
+  const toast = useToast();
 
   const handleChange = (event: { target: { value: string } }) =>
     setSearchText(event.target.value);
 
   const handleClick = () => {
-    axios
-      .get(
-        `/api/proxy/novelapi/api?out=json&lim=20&of=t-n-u-w-s-bg-g-gl-nt-ga-e-i-iti-mp-nu&word=${searchText}`
-      )
-      .then((r) => {
-        setBooks(r.data);
-      });
+    if (!searchText) {
+      setErrorToastMessage("条件を入力してください。");
+    } else {
+      axios
+        .get(
+          `/api/proxy/novelapi/api?out=json&lim=20&of=t-n-u-w-s-bg-g-gl-nt-ga-e-i-iti-mp-nu&word=${searchText}`
+        )
+        .then((r) => {
+          setBooks(r.data);
+        })
+        .catch(() => {
+          setErrorToastMessage("データの取得に失敗しました。");
+        });
+    }
   };
 
+  const handleKeyPress = () => {
+    handleClick();
+  };
+
+  useEffect(() => {
+    if (errorToastMessage) {
+      toast({
+        title: errorToastMessage,
+        status: "error",
+        position: "top",
+        duration: 2000,
+        isClosable: true,
+      });
+    }
+  }, [errorToastMessage, toast]);
+
   const rankingList = (): JSX.Element[] | JSX.Element => {
-    if (!books.length) return <Box>検索するとこちらに表示されます。</Box>;
+    if (!books.length) return <Box>検索結果はこちらに表示されます。</Box>;
     if (!books[0].allcount) return <Box>該当の作品がありません</Box>;
     return books.map((narouData: NarouBookData, i: number) => {
       if (i === 0) return <div key={i}></div>;
@@ -39,7 +64,15 @@ export default function Search(): JSX.Element {
         <Box mb={{ base: 50 }}>
           <Stack direction="row" spacing={4} mb={5} mr={{ md: 8 }}>
             <Box>
-              <Input onChange={handleChange} placeholder="転生　勇者" />
+              <Input
+                onChange={handleChange}
+                onKeyPress={(e) => {
+                  if (e.key == "Enter") {
+                    handleKeyPress();
+                  }
+                }}
+                placeholder="転生　勇者"
+              />
               <Text fontSize="xs">
                 複数条件で検索する場合はスペースで区切ってください。
               </Text>
