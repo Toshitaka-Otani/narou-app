1b20c18c73809ea55e6a1ab89678ac415c60ad0b
ToshitakaOtani <v.t.tanitani@gmail.com>
2022-07-18 01:47:46 +0900

    不要コード削除

--- pages/search.tsx
+++ pages/search.tsx
@@ -4,18 +4,13 @@ import React, { useState } from "react";
 import { BookData } from "../components/BookData";
 
 export default function Search(): JSX.Element {
-  // const [checkedItems, setCheckedItems] = useState([]);
   const [searchText, setSearchText] = useState<string>("");
   const [books, setBooks] = useState<NarouBookData[]>([]);
-  // const allChecked = checkedItems.every(Boolean);
-  // const isIndeterminate = checkedItems.some(Boolean) && !allChecked;
-  // const searchCategory = ["タイトル", "あらすじ", "キーワード", "作者名"];
 
   const handleChange = (event: { target: { value: string } }) =>
     setSearchText(event.target.value);
 
   const handleClick = () => {
-    console.log("aaaaaa");
     axios
       .get(
         `/api/proxy/novelapi/api?out=json&lim=20&of=t-n-u-w-s-bg-g-gl-nt-ga-e-i-iti-mp-nu&word=${searchText}`
@@ -24,17 +19,6 @@ export default function Search(): JSX.Element {
         setBooks(r.data);
       });
   };
-
-  // const handleCheck = (event: React.ChangeEvent<HTMLInputElement>) => {
-  //   let updateList = [...checkedItems];
-  //   if (event.target.checked) {
-  //     updateList = [...checkedItems, event.target.checked];
-  //   } else {
-  //     updateList.splice(checkedItems.indexOf(event.target.checked));
-  //   }
-  //   console.log(updateList);
-  //   setCheckedItems(updateList);
-  // };
 
   const rankingList = (): JSX.Element[] | JSX.Element => {
     if (!books.length) return <Box>検索するとこちらに表示されます。</Box>;
@@ -62,68 +46,6 @@ export default function Search(): JSX.Element {
             </Box>
             <Button onClick={handleClick}>検索</Button>
           </Stack>
-          {/* <Text>検索条件を選択してください</Text>
-          <Checkbox
-            isChecked={allChecked}
-            isIndeterminate={isIndeterminate}
-            onChange={(e) =>
-              setCheckedItems([
-                e.target.checked,
-                e.target.checked,
-                e.target.checked,
-                e.target.checked,
-              ])
-            }
-          >
-            すべて選択
-          </Checkbox>
-          <Stack pl={6} mt={1} spacing={1}>
-            {searchCategory.map((category, i) => {
-              console.log("aaa");
-              return (
-                <Checkbox
-                  key={i}
-                  isChecked={checkedItems[i]}
-                  onChange={handleCheck}
-                >
-                  {category}
-                </Checkbox>
-              );
-            })}
-
-            <Checkbox
-              isChecked={checkedItems[0]}
-              onChange={(e) => {
-                setCheckedItems([checkedItems[0], e.target.checked]);
-              }}
-            >
-              タイトル
-            </Checkbox>
-            <Checkbox
-              isChecked={checkedItems[1]}
-              onChange={(e) =>
-                setCheckedItems([checkedItems[1], e.target.checked])
-              }
-            >
-              あらすじ
-            </Checkbox>
-            <Checkbox
-              isChecked={checkedItems[2]}
-              onChange={(e) =>
-                setCheckedItems([checkedItems[2], e.target.checked])
-              }
-            >
-              キーワード
-            </Checkbox>
-            <Checkbox
-              isChecked={checkedItems[3]}
-              onChange={(e) =>
-                setCheckedItems([checkedItems[3], e.target.checked])
-              }
-            >
-              作者名
-            </Checkbox>
-          </Stack> */}
         </Box>
         <Box
           justifyContent={{ md: "center" }}
